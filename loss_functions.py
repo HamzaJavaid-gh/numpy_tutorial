7d07f239b2ce13b9ebb4de2a17afa9a729aba209
HamzaJavaid-gh <hamzajaved479@gmail.com>
2023-01-09 01:02:25 +0500

    [ADD] contrastive loss for image similarity

--- loss_functions.py
+++ loss_functions.py
@@ -27,3 +27,14 @@ def js_divergence(y_true, y_pred):
     m_val = 1/(2*(y_true+y_pred))
     y_out = 0.5 * kl_divergence(y_true, m_val) + 0.5 * kl_divergence(y_pred, m_val)
     return y_out
+
+def euclidean_dis(feat_v1, feat_v2):
+    y_out = np.sum(np.square(feat_v1 - feat_v2))
+    return y_out
+
+MARGIN = 10
+def contrastive_loss(feat_v1, feat_v2, y_true):
+    y_out = (y_true)*np.square(euclidean_dis(feat_v1, feat_v2)) \
+        (1 - y_true)*np.max([MARGIN-euclidean_dis(feat_v1, feat_v2),0])
+    return y_out
+
