c6242fd233cf5fc51ff52eaa5757d36496871eb1
HamzaJavaid-gh <hamzajaved479@gmail.com>
2023-01-11 21:27:33 +0500

    [ADD] exercise for conditionals

--- conditionals.py
+++ conditionals.py
@@ -28,3 +28,40 @@ print(arr_in2[ind_arr])
 # or
 print(arr_in2[np.where(arr_in1>4)])
 
+
+# Explanation of np.where, np.argwhere
+
+# np.where only returns the indices
+# on 1d array
+arr_1d = np.arange(1,10,1)
+print(arr_1d)
+arr_con = np.where(arr_1d>6)
+print(arr_con)
+print(arr_1d[arr_con])
+
+# if you want to replace elements based on a condition
+arr_con2 = np.where(arr_1d>6,0,arr_1d)
+print(arr_con2)
+
+arr_con3 = np.where(arr_1d>4,5,8)
+print(arr_con3)
+
+
+# exrcises for conditionals within the array
+
+arr_1d = np.arange(10,20,1)
+
+arr_con = arr_1d[arr_1d>15]  #equivalent to arr_1d = arr_1d[np.where(arr_1d>15)]
+print(arr_con)
+
+arr_1d[arr_1d>15] = 2   #equivalent to np.where(arr1d>15,2,arr_1d)
+print(arr_1d)
+arr_1d[arr_1d<15] = 0
+print(arr_1d)
+
+
+# arr_1 = np.arange(9).reshape(3,3)
+# print(arr_1)
+#
+# arr_1_sec = np.where(arr_1>4)
+# print(arr_1_sec)
